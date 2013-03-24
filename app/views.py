cfce8c3a9e77bed4d4dfa18effc21a78e2538b92
Simon Jagoe <simon@simonjagoe.com>
2013-03-24 18:42:06 +0000

    Removed unused views

--- app/views.py
+++ app/views.py
@@ -75,25 +75,3 @@ def logout(request):
     """Logs out user"""
     auth_logout(request)
     return HttpResponseRedirect('/github-jenkins')
-
-
-def form(request):
-    if request.method == 'POST' and request.POST.get('username'):
-        name = setting('SOCIAL_AUTH_PARTIAL_PIPELINE_KEY', 'partial_pipeline')
-        request.session['saved_username'] = request.POST['username']
-        backend = request.session[name]['backend']
-        return redirect('socialauth_complete', backend=backend)
-    return render_to_response('form.html', {}, RequestContext(request))
-
-
-def form2(request):
-    if request.method == 'POST' and request.POST.get('first_name'):
-        request.session['saved_first_name'] = request.POST['first_name']
-        name = setting('SOCIAL_AUTH_PARTIAL_PIPELINE_KEY', 'partial_pipeline')
-        backend = request.session[name]['backend']
-        return redirect('socialauth_complete', backend=backend)
-    return render_to_response('form2.html', {}, RequestContext(request))
-
-
-def close_login_popup(request):
-    return render_to_response('close_popup.html', {}, RequestContext(request))
